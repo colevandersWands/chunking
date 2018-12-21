aa8a185bc38794ab62a0fd66dd4a4e43f36980f5
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-22 00:45:48 +0100

    better-chunking

--- template.js
+++ template.js
@@ -1,5 +1,4 @@
-// need better examples than this, not much to show here
-// but the steps are in place
+// how good is this example
 
 console.log('--- og code ---');
 {
@@ -42,24 +41,22 @@ console.log('--- chunked ---');
   let a = 3;
   const b = true;
 
-  // chunked code
+  // chunked code - using labeled blocks?
   //  strategically meaningful code extracted to a block, if appropriate
   //  descriptive variables assigned at each step
   //  the code should read something like a text
   // ie.
-  let condition; { // = typeof a || b <= a
-    const step_1 = typeof a;
-    const step_2 = b <= a;
-    const step_3 = step_1 || step_2;
-    condition = step_3;
-  }
-  if (condition) {
-    let new_a; { // a += b % a
-      const step_1 = b % a
-      const step_2 = a + step_1
-      new_a = step_2
-    };
-    a = new_a
+  _conditionally_update_a: {
+    
+    const condition = typeof a || b <= a;
+    if (condition) {
+
+      _determine_new_a: {
+        let new_a = a + b % a
+        a = new_a
+      }
+
+    }
   }
 
   console.log(a, b)
