4acc0499e8d626eb4a0e7cd6d09a0ee2565bca1f
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-25 04:41:46 +0100

    sooo-better

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
+the questions
+* identifying chunks
+    * Identifying chunks is largely about tracking variable usage
+    * what are variable values before the chunk?
+    * what are variable values after the chunk?
+    * has a new strategic variable been created?
+    * utility variables are often used only within a chunk
+* assessing a chunk's behavior
+    * what values changed & how
+    * where were those values assigned previously (before chunk)
+    * where are they used next (after chunk)
+
+
 learning objectives
 * higher level analysis of variable usage
     * strategic: storing or compiling important states on the way to finished result
     * utility: anything else
 * identifying chunks of code that do one complete & meaningful task
-* isolating chunks, trying to make them rely on as few external variables as possible
+* relating strategy and implementation
 * control structures (loops, conditionals): when is the whole thing a chunk, and when is the body a chunk
+
+> note: chunk variables should be needed for anything but logging strategy.
 
 exercises:
 * some snippets and the template
--- template.js
+++ template.js
@@ -1,67 +1,121 @@
-// how good is this example
+// can you find an example that won't update a?
+//  find one, or prove why you can't
 
 console.log('--- og code ---');
-{
-  let a = 3;
-  const b = true;
+  function code(a, b){
 
-  if (typeof a || b <= a) {
-    a += b % a
-  }
-
-  console.log(a, b)
-}
-
-console.log('--- fully expanded ---');
-{
-  let a = 3;
-  const b = true;
-
-  // expand the code to one operation per line
-  let condition; { // = typeof a || b <= a
-    const step_1 = typeof a;
-    const step_2 = b <= a;
-    const step_3 = step_1 || step_2;
-    condition = step_3;
-  }
-  if (condition) {
-    let new_a; { // a += b % a
-      const step_1 = b % a
-      const step_2 = a + step_1
-      new_a = step_2
-    };
-    a = new_a
-  }
-
-  console.log(a, b)
-}
-
-console.log('--- chunked ---');
-{
-  let a = 3;
-  const b = true;
-
-  // chunked code - using labeled blocks?
-  //  strategically meaningful code extracted to a block, if appropriate
-  //  descriptive variables assigned at each step
-  //  the code should read something like a text
-  // ie.
-  _conditionally_update_a: {
-    
-    const condition = typeof a || b <= a;
-    if (condition) {
-
-      _determine_new_a: {
-        let new_a = a + b % a
-        a = new_a
-      }
-
+    if (typeof a || b <= a) {
+      a += b % a
     }
+
+    return a;
   }
 
-  console.log(a, b)
-}  
-  // not yet fully refactoring into functions
+console.log('--- test cases ---')
+
+  const test_cases = [
+      {name: '3, 5', args: [3,5], expected: 5},
+      {name: '9, 5', args: [9,5], expected: 14},
+      {name: 'true, false', args: [true,false], expected: 1},
+      {name: '"3", "5"', args: ['3','5'], expected: '32'},
+      // {name: '0, 0', args: [0,0], expected: NaN},
+    ]
+  run_tests(code, test_cases)
+
+console.log('--- expand as needed ---');
+  // expand as much as needed to identify chunks
+  // this may not be at all!
+  function pand(a, b){ 
+
+    let condition; { // = typeof a || b <= a
+      const step_1 = typeof a;
+      const step_2 = b <= a;
+      const step_3 = step_1 || step_2;
+      const step_4 = Boolean(step_3)
+      condition = step_4;
+    }
+    if (condition) {
+      let new_a; { // a += b % a
+        const step_1 = b % a
+        const step_2 = a + step_1
+        new_a = step_2
+      };
+      a = new_a
+    }
+
+    return a;
+  }
+  run_tests(pand, test_cases)
+
+console.log('--- chunk ---')
+  function chunked(a, b){
+    let update_a; {
+      const step_1 = typeof a;
+      const step_2 = b <= a;
+      const step_3 = step_1 || step_2;
+      const step_4 = Boolean(step_3)
+      condition = step_4;
+    update_a = condition };
+
+    if (update_a) {
+      let set_new_a; {
+        a += b % a
+      set_new_a = a}    
+    }
+
+    return a;
+  } 
+  run_tests(chunked, test_cases)
+
+console.log('--- chunk-logged ---')
+  function logged(a, b){                     const log = [];
+
+    let update_a; {
+      const step_1 = typeof a;
+      const step_2 = b <= a;
+      const step_3 = step_1 || step_2;
+      const step_4 = Boolean(step_3)
+      condition = step_4;
+    update_a = condition };                  log.push({update_a})
+
+    if (update_a) {
+      let new_a; {
+        a += b % a
+      new_a = a}                             log.push({new_a})
+    }
+
+    return {a, log};
+  } 
+  log_reports(logged, test_cases)
 
 
+// testing utils
+function run_tests(_target, _cases) {
+  for (let t_case of _cases) {
+    let expected = t_case.expected;
+    let actual = _target(... t_case.args, false);
 
+    let pass;
+    if (typeof expected === 'object') {
+      const _actual = JSON.stringify(actual);
+      const _expected = JSON.stringify(expected);
+      pass = _actual === _expected;
+    } else {
+      pass = actual === expected;
+    };
+
+    if (!pass) {
+      console.log(`${t_case.name}: \n` + 
+          `   actual: {${typeof actual}, ${actual}} \n` +
+          `   expected: {${typeof expected}, ${expected}}`);
+    };
+  };
+};
+
+function log_reports(_target, _cases) {
+  const report = {}
+  for (let t_case of _cases) {
+     report[t_case.name] = _target(...t_case.args) 
+  }
+  console.log(report)
+}
